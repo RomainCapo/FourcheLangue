f6df7e8901074b561e86d78fa9b0e0fbb78661ec
Romain Capocasale <romain.capocasale@he-arc.ch>
2019-05-10 11:27:44 +0200

    add debut hash function

--- hashtable.js
+++ hashtable.js
@@ -1,15 +1,22 @@
 class HashTable{
   constructor(){
-    let array = [''];
+    let array = ['3', '4', '5', '6'];
     let length = array.length * 2;
   }
 
   fn(cle){
     let hash = 0;
     do {
-      hash += cle%10;
-      cle /= length;
-    } while (cle>0);
-    return hash%length;
+      hash += cle.charCodeAt()%this.length;
+      console.log(hash);
+
+      cle = cle.slice(0, cle.length-1);
+      console.log(cle);
+    } while (cle.length > 0);
+    return hash%this.length;
+  }
+
+  test(){
+    return "A".charCodeAt();
   }
 }
