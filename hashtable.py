6a88eb16efa2cebe0cc7d702634d03996ad00890
Romain Capocasale <romain.capocasale@he-arc.ch>
2019-05-12 22:10:23 +0200

    finish hashtable generating method

--- hashtable.py
+++ hashtable.py
@@ -7,39 +7,63 @@ INF2dlm-a
 2018-2019
 """
 import os.path
+import json
 
 class HashTable:
     def __init__(self):
         self.words = []
-        self.length = 20000
+        self.hashtable = []
 
     def djb2(self, string):
         hash = 0 
         for x in string:
             hash = ((hash << 5) + hash) + ord(x)
-        return hash
+        return hash % self.length
 
     def fn(self, string):
         hash = 0
-        i = 1
+        i = 0
+        c = 42
         for x in string:
-            hash = hash + (ord(x) *i)
+            hash = hash + (ord(x) * (c**i))
             i = i + 1
         return hash % self.length 
 
-    def _openFile(self, filename):
+    def _openFile(self, filename, mode):
         if os.path.exists(filename):
-            f = open(filename, 'r')
-            print(f.readlines())
+            return open(filename, mode)
+
+    def _initList(self):
+        self.hashtable = [[] for x in range(self.length)]
 
     def generateHashTable(self, filename):
-            pass
-            #for word in f.readlines():
-             #   print(word)
+            f = self._openFile(filename, 'r')
+            reader = f.read().splitlines()
+            self.length = len(reader) * 2
+            self._initList()
+            print(self.length)
+
+            for word in reader:
+                hash = self.fn(word)
+                print("word : " + word + ", hash : " + str(hash))
+                
+                self.hashtable[hash].append(word)
+
+    def exportHashTable(self, filename):
+        f = self._openFile(filename, 'w')
+        out = {}       
+        
+        print(out)
+            
+
         
 
-french_path = "C:/DEV/2eme/algo/projetalgo/dict/french.txt"
+french_path = "C:/Users/Romain/Desktop/He-Arc/Algorithme/projetalgo/dict/french.txt"
 #french_path = "dict/french.txt"
 
 t = HashTable()
-t._openFile(french_path)
+t.generateHashTable(french_path)
+
+print(t.hashtable)
+
+t.exportHashTable('fes.txt')
