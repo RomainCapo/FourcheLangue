4a8f5a4ee696e2d04c322f3801017e3c49e745aa
vincent.moulin1 <vincent.moulin@he-arc.ch>
2019-05-14 14:12:36 +0200

    Added displaying flag with words

--- script.js
+++ script.js
@@ -6,6 +6,19 @@
 * Projet cours d'algorithme
 */
 
+let hashFilePaths = [['french', 'hash/french.json'], ['english','hash/english.json'], ['deutsch','hash/deutsch.json'], ['italiano', 'hash/italiano.json'], ['spain', 'hash/espagnol.json'], ['denmark', 'hash/dansk.json'], ['norsk', 'hash/norsk.json']];
+
+let hashtables = [];
+let lang = [];
+
+hashFilePaths.forEach(function(e){
+  hashtables[e[0]] = new HashTable(e[1]);
+  lang[e[0]] = 0;
+})
+//console.log(hashtables);
+//console.log(lang);
+
+
 function init() {
   document.getElementById("language_infos").style.visibility = "hidden";
   document.getElementById("content_textarea").focus();
@@ -18,46 +31,38 @@ function getContent() {
   let values_clear = values.replace(regex, '');
   let values_list = values_clear.split(' ');
 
-  document.getElementById("value").innerHTML = values_list;
+  //document.getElementById("value").innerHTML = values_list;
 
-  if (values_list[0] == "test") { changeImg("german"); } else { changeImg("italian"); }
+  findLang(values_list);
 }
 
 function changeImg(language) {
 	document.getElementById("language_infos").style.visibility = "visible";
-	switch(language) {
-	  case "german":
-	    document.getElementById("img_lang").src="flags/german.png";
-	    break;
-	  case "italian":
-	    document.getElementById("img_lang").src="flags/italian.png";
-	    break;
-	  case "english":
-	    document.getElementById("img_lang").src="flags/english.png";
-	    break;
-	  case "spain":
-	    document.getElementById("img_lang").src="flags/spain.png";
-	    break;
-	  case "french":
-	    document.getElementById("img_lang").src="flags/french.png";
-	    break;
-	  default:
-	} 
+	document.getElementById("img_lang").src="flags/" + language + ".png";
 }
 
+function findLang(values_list) {
+	for (let key in lang) {
+	  lang[key] = 0;
+	}
 
-(function(){
-	let hashFilePaths = [['french', 'hash/french.json'], ['english','hash/english.json'], ['deutsch','hash/deutsch.json'], ['italiano', 'hash/italiano.json']];
-
-	let hashtables = [];
-	let lang = [];
-
-	hashFilePaths.forEach(function(e){
-	  hashtables[e[0]] = new HashTable(e[1]);
-	  lang[e[0]] = [];
-	})
-	console.log(hashtables);
+	for(let i in values_list) {
+	 	for(let key_lang in lang) {
+		 	if(hashtables[key_lang].wordInHashTable(values_list[i])) {
+		 		lang[key_lang]++;
+		 	}
+	 	}
+	}
+	
 	console.log(lang);
-})();
+
+	chooseLang(lang);
+}
+
+function chooseLang(lang) {
+	let key = Object.keys(lang).reduce(function(a, b){ return lang[a] > lang[b] ? a : b });
+	changeImg(key);
+}
+
 
 
