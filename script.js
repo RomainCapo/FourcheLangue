905a1ac58dd179d1b5c46f488c785db6764ae6d3
Romain Capocasale <romain.capocasa@he-arc.ch>
2019-05-14 10:46:32 +0200

    add hashtable in js

--- script.js
+++ script.js
@@ -6,4 +6,11 @@
 * Projet cours d'algorithme
 */
 
-let hashtable = new HashTable("hash/french.json");
+let hashFilePaths = [['french', 'hash/french.json'], ['english','hash/english.json'], ['deutsch','hash/deutsch.json'], ['italiano', 'hash/italiano.json']];
+
+let hashtables = [];
+
+hashFilePaths.forEach(function(e){
+  hashtables[e[0]] = new HashTable(e[1]);
+})
+console.log(hashtables);
